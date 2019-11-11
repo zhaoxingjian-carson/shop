91cd44788c3e60808a899b0e0f968215d05e2f14
zhaoxingjian-carson <719222106@qq.com>
2019-11-12 05:27:28 +0800

    微信公众号接口代码提交

--- shop-parent/shop-service-impl/shop-weixin-service-impl/src/main/java/com/carson/WeixinAppServer.java
+++ shop-parent/shop-service-impl/shop-weixin-service-impl/src/main/java/com/carson/WeixinAppServer.java
@@ -1,10 +1,7 @@
-package com.carson.weixin;
+package com.carson;
 
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
-import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author zhaoxingjian
@@ -12,9 +9,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @date 2019/11/7 19:56
  */
 @SpringBootApplication
-@EnableEurekaClient
-@EnableSwagger2Doc
-@EnableApolloConfig
+//@EnableEurekaClient
+//@EnableSwagger2Doc
+//@EnableApolloConfig
 public class WeixinAppServer {
     public static void main(String[] args) {
         SpringApplication.run(WeixinAppServer.class, args);
